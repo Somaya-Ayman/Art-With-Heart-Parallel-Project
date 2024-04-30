2bf20700ba83ee2d40727accb2899664e522d0a9
MightyMaya <mayasharaf2@gmail.com>
2024-04-30 14:30:30 +0300

    90:115

--- parall prj/parallelprj/app/views.py
+++ parall prj/parallelprj/app/views.py
@@ -1,5 +1,5 @@
 
-
+from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
 from . models import Product
@@ -11,4 +11,16 @@ def home(request):
 class CategoryView(View):
     def get(self,request,val):
         product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
         return render(request,"app/category.html",locals())
+
+class CategoryTitle(View):
+    def get(self,request,val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category= product[0].category).values('title')
+        return render(request,"app/category.html",locals())
+
+class ProductDetail(View): 
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)  
+        return render(request,"app/productdetail.html",locals())
