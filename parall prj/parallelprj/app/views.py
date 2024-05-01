3771b67cbe3387047c826a4e1e29c273a7a26b5e
MightyMaya <mayasharaf2@gmail.com>
2024-05-01 11:40:09 +0300

    90:130

--- parall prj/parallelprj/app/views.py
+++ parall prj/parallelprj/app/views.py
@@ -3,6 +3,7 @@ from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
 from . models import Product
+from . forms import CustomerRegistrationForm
 # Create your views here.
 
 def home(request):
@@ -30,3 +31,9 @@ class ProductDetail(View):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)  
         return render(request,"app/productdetail.html",locals())
+    
+class CustomerRegistrationView(View): 
+    def get(self, request):
+        form = CustomerRegistrationForm() 
+        return render(request,"app/customerregistration.html",locals())
+        
